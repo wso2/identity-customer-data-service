79167838b9f023fa2ec5b3e0c0db3f305f662fef
Maduranga Siriwardena <maduranga.siriwardena@gmail.com>
2025-05-10 09:40:50 +0530

    Remove unwanted dependencies

--- go_server_gen.go
+++
@@ -1,3 +0,0 @@
-package identity_customer_data_service
-
-//go:generate go tool oapi-codegen -config cfg.yaml api/customer-data-service.yaml
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -4,49 +4,13 @@ import (
 	"encoding/json"
 	"errors" // Standard Go errors package
 	"fmt"
-	"log"
 	"net/http"
 	"strings"
 
-	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"github.com/wso2/identity-customer-data-service/internal/system/constants"
 	customerrors "github.com/wso2/identity-customer-data-service/internal/system/errors" // Alias for the custom errors
 	// package
 )
-
-func HandleError(c *gin.Context, err error) {
-	traceID := c.GetString("traceId")
-	if traceID == "" {
-		traceID = uuid.NewString()
-	}
-
-	switch e := err.(type) {
-	case *customerrors.ClientError:
-		c.JSON(e.StatusCode, gin.H{
-			"error_code":        e.Code,
-			"error_message":     e.Message,
-			"error_description": e.Description,
-			"traceId":           traceID,
-		})
-	case *customerrors.ServerError:
-		log.Printf("[ERROR] %s ", e.Error())
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error_code":        e.Code,
-			"error_message":     e.Message,
-			"error_description": e.Description,
-			"traceId":           traceID,
-		})
-	default:
-		log.Printf("[ERROR] %s ", e.Error())
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error_code":        "CDS-50000",
-			"error_message":     "Internal Server Error",
-			"error_description": "An unexpected error occurred.",
-			"traceId":           traceID,
-		})
-	}
-}
 
 // HandleHTTPError sends an HTTP error response based on the provided error
 func HandleHTTPError(w http.ResponseWriter, err error) {
