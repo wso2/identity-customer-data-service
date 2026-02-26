10470b562e1d5fa020882348960a67da9d96eac9
VivekVinushanth <cvivekvinushanth@gmail.com>
2026-02-26 19:13:08 +0530

    fix(schema): revert unintended

--- internal/profile_schema/model/profile_schema_api.go
+++ internal/profile_schema/model/profile_schema_api.go
@@ -22,16 +22,3 @@ type ProfileSchemaSync struct {
 	OrgId string `json:"orgHandle" bson:"orgHandle"`
 	Event string `json:"event" bson:"event"`
 }
-
-type ProfileSchemaAddAttribute struct {
-	AttributeId           string           `json:"attribute_id" bson:"attribute_id"`
-	AttributeName         string           `json:"attribute_name" bson:"attribute_name" binding:"required"`
-	DisplayName           string           `json:"display_name,omitempty" bson:"display_name,omitempty"`
-	ValueType             string           `json:"value_type" bson:"value_type" binding:"required"`
-	MergeStrategy         string           `json:"merge_strategy" bson:"merge_strategy" binding:"required"`
-	Mutability            string           `json:"mutability" bson:"mutability"`
-	ApplicationIdentifier string           `json:"application_identifier,omitempty" bson:"application_identifier,omitempty"`
-	MultiValued           bool             `json:"multi_valued,omitempty" bson:"multi_valued,omitempty"`         // Means the data type is an array of chosen data type
-	CanonicalValues       []CanonicalValue `json:"canonical_values,omitempty" bson:"canonical_values,omitempty"` // String of options for the attribute
-	SubAttributes         []SubAttribute   `json:"sub_attributes,omitempty" bson:"sub_attributes,omitempty"`     // If the datatype is object
-}
